776e74b1cf8cad2b10433154661e047ca5dc801b
ViniOlson <viniolson@hotmail.com>
2022-09-02 12:40:10 -0300

    first commit

---
+++ variaveis.js
@@ -0,0 +1,48 @@
+ 
+ // Declaração de variaveis
+ var name;
+
+// Atribuição de valoras em variaveis
+
+nome = 'Vini'; //String
+
+// Tipo de dado inserido na variavel
+
+let age = 20; // Number
+let isHuman = true; //Boolean
+
+//Agrupamento de declarações
+
+let idade, eHumano;
+idade = 20;
+eHumano = true;
+
+console.log(age, isHuman);
+
+//Multiplos argumentos na função
+
+console.log('O '+nome+' tem '+age+' anos');
+
+//Object
+
+const person ={
+    nomeOb: 'Vico',
+    ageOb: '24',
+    weight: '90',
+    isADmin: true
+}
+
+console.log(person)
+
+
+//Array
+
+const animals = [
+    'Lion',
+    'Monkey',
+    'Cat',
+    'Dog'
+]
+
+console.log(animals)
+
